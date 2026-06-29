1fde7bf0347ed68756faaec1f5a2c5da4a14a4f7
choreocoder <otlilelebelo707@gmail.com>
2026-06-29 21:20:50 +0200

    feat: added DiscipleshipGroup class and enum to classify type..

---
+++ models/DiscipleshipGroup.java
@@ -0,0 +1,51 @@
+package models;
+
+public class DiscipleshipGroup {
+    private int groupId;
+    private String groupName;
+    private String residentialArea;
+    private int leaderId;
+    private GroupType groupType;
+
+
+    public DiscipleshipGroup(int groupId, String groupName, String residentialArea, int leaderId){
+        this.groupId = groupId;
+        this.groupName = groupName;
+        this.residentialArea = residentialArea;
+        this.leaderId = leaderId;
+        this.groupType = GroupType.MIXED;
+
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public String getGroupName(){
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public String getResidentialArea(){
+        return residentialArea;
+    }
+
+    public void setResidentialArea(String residentialArea) {
+        this.residentialArea = residentialArea;
+    }
+
+    public int getLeaderId() {
+        return leaderId;
+    }
+
+    public GroupType getGroupType() {
+        return groupType;
+    }
+
+    public void setGroupType(GroupType groupType) {
+        this.groupType = groupType;
+    }
+}
---
+++ models/GroupType.java
@@ -0,0 +1,8 @@
+package models;
+
+public enum GroupType {
+    MENS,
+    WOMENS,
+    YOUTH,
+    MIXED
+}
